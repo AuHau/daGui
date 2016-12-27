44b2f775aa365702a60c5f9c17632be1d88fbdc0
Adam Uhlir <uhlir.a@gmail.com>
2016-12-27 13:53:29 +0100

    Basic Syntax highlighting

--- app/core/renderer/components/editor/CodeView.js
+++ app/core/renderer/components/editor/CodeView.js
@@ -1,10 +1,42 @@
 // @flow
 import React, {Component} from 'react';
+import ace from 'brace';
+import 'brace/mode/python';
+import 'brace/theme/chrome';
 
 import levels, {classTranslation, textTranslation} from 'graph/ErrorLevel';
 import styles from './CodeView.scss';
 
 export default class CodeView extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.editor = null;
+  }
+
+  componentDidMount(){
+    this.editor = ace.edit('aceCodeEditor');
+    this.editor.getSession().setMode('ace/mode/python');
+    this.editor.setTheme('ace/theme/chrome');
+    this.editor.setValue(this.props.code);
+    this.editor.setReadOnly(true);
+    this.editor.clearSelection();
+  }
+
+  componentDidUpdate(){
+    this.editor.setValue(this.props.code);
+    this.editor.clearSelection();
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        {this.renderErrors()}
+        <div className={styles.codeEditor} id="aceCodeEditor"></div>
+      </div>
+    );
+  }
 
   renderErrors(){
     if(!this.props.errors || !this.props.errors.length) return;
@@ -18,17 +50,6 @@ export default class CodeView extends Component {
       <h3>Errors found!</h3>
       {detailedErrors}
       </div>)
-  }
-
-  render() {
-    return (
-      <div className={styles.container}>
-        {this.renderErrors()}
-        <div className={styles.code} data-selectable>
-          {this.props.code}
-        </div>
-      </div>
-    );
   }
 }
 
