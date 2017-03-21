eb75853a10a7592f25b6c95fbba28bddf33d3b5f
Adam Uhlir <uhlir.a@gmail.com>
2017-03-21 20:01:01 +0100

    Support for dragging of multiple selected nodes

--- app/core/renderer/components/editor/canvas_components/Nodes.js
+++ app/core/renderer/components/editor/canvas_components/Nodes.js
@@ -6,28 +6,45 @@ export default class Nodes extends CanvasComponentBase{
   }
 
   init() {
-    this.graph = this.canvas.graph;
+    super.init();
+    this.selected = this.canvas.selected;
+
     this.canvas.paper.on('cell:pointerdown blank:pointerdown', this.onPointerDown.bind(this));
     this.canvas.paper.on('cell:pointerup', this.onPointerUp.bind(this));
+    this.canvas.paper.el.addEventListener('mousemove', this.onMultipleDrag.bind(this));
     document.addEventListener('keyup', this.onDeleteKey.bind(this));
   }
 
+  onMultipleDrag(e){
+    if(!this.startingPointerPosition || !this.startingElement) return;
+
+    if(this.selected.size > 1 && this.selected.has(this.startingElement.id)
+      && Math.abs(this.startingPointerPosition.x - e.clientX) < this.canvas.CLICK_TRESHOLD
+      && Math.abs(this.startingPointerPosition.y - e.clientY) < this.canvas.CLICK_TRESHOLD) {
+
+      for(let selectedNid of this.selected){
+        if(this.startingElement.id == selectedNid) continue;
+        this.startingElement.embed(this.graph.getCell(selectedNid))
+      }
+    }
+  }
 
   // TODO: [BUG/Medium] When dragging node Z value should be the highest in graph
   onPointerDown(cellView, e, x, y){
     if(!y){ // blank click
       this.startingPointerPosition = {x: cellView.clientX, y: cellView.clientY};
     }else{ // cell click
-      this.startingPointerPosition = {x, y};
+      this.startingPointerPosition = {x: e.clientX, y: e.clientY};
       this.freeze();
     }
+    this.startingElement = cellView.model;
   }
 
   onPointerUp(cellView, e, x, y){
     if(!e.target || e.target.type != 'text') this.unfreeze();
 
-    if(Math.abs(this.startingPointerPosition.x - x) < this.canvas.CLICK_TRESHOLD
-      && Math.abs(this.startingPointerPosition.y - y) < this.canvas.CLICK_TRESHOLD) {
+    if(Math.abs(this.startingPointerPosition.x - e.clientX) < this.canvas.CLICK_TRESHOLD
+      && Math.abs(this.startingPointerPosition.y - e.clientY) < this.canvas.CLICK_TRESHOLD) {
       // Click
       if(e.target && e.target.type == 'text'){
         e.target.focus(); // Variable input
@@ -35,7 +52,16 @@ export default class Nodes extends CanvasComponentBase{
     }else{
       // Drag node
       if(cellView.model.isElement()){
-        this.onNodeMove(cellView);
+        const embeddedCels = this.startingElement.getEmbeddedCells();
+        if(embeddedCels && embeddedCels.length > 0){
+          for(let cell of embeddedCels){  // TODO: [Medium] Batch node moving
+            this.startingElement.unembed(cell);
+            this.onNodeMove(cell);
+          }
+          this.onNodeMove(this.startingElement);
+        }else{
+          this.onNodeMove(cellView.model);
+        }
       }
     }
 
@@ -56,10 +82,11 @@ export default class Nodes extends CanvasComponentBase{
     }
   }
 
-  onNodeMove(cellView){
-    if(cellView.model.attributes.type != 'link'){
-      const newPosition = cellView.model.attributes.position;
-      this.call('onNodeMove', cellView.model.id, newPosition.x, newPosition.y);
+  onNodeMove(cell){
+    if(cell.attributes.type != 'link'){
+      const newPosition = cell.attributes.position;
+      this.ignoreAction();
+      this.call('onNodeMove', cell.id, newPosition.x, newPosition.y);
     }
   }
 }
