a49bef36fa4d3cc90da5b40b08bbff1bc55ac10f
Adam Uhlir <uhlir.a@gmail.com>
2017-05-15 12:41:24 +0200

    Adding custom icons + finishing Menu styling

--- app/core/renderer/components/layout/Menu.js
+++ app/core/renderer/components/layout/Menu.js
@@ -50,31 +50,32 @@ class Menu extends Component {
     return (
       <div className={styles.container}>
         <ul className={styles.left}>
-          <li><a href="#" onClick={this.props.onNew}><i className="fa fa-file-o"/></a></li>
-          <li><a href="#" onClick={this.props.onSave}><i className="fa fa-save"/></a></li>
-          <li><a href="#" onClick={this.props.onOpen}><i className="fa fa-folder-open-o"/></a></li>
+          <li><a href="#" onClick={this.props.onNew}><i className="icon-file"/></a></li>
+          <li><a href="#" onClick={this.props.onOpen}><i className="icon-open"/></a></li>
+          <li><a href="#" onClick={this.props.onSave}><i className="icon-save"/></a></li>
+          <li><a href="#"><i className="icon-screenshot"/></a></li>
         </ul>
         <ul className={styles.left}>
-          <li><a href="#" onClick={this.props.onCopy}><i className="fa fa-files-o"/></a></li>
-          <li><a href="#" onClick={this.props.onCut}><i className="fa fa-scissors"/></a></li>
-          <li><a href="#" onClick={this.props.onPaste}><i className="fa fa-clipboard"/></a></li>
+          <li><a href="#" onClick={this.props.onCopy}><i className="icon-copy"/></a></li>
+          <li><a href="#" onClick={this.props.onCut}><i className="icon-take-out"/></a></li>
+          <li><a href="#" onClick={this.props.onPaste}><i className="icon-paste"/></a></li>
         </ul>
         <ul className={styles.left}>
-          <li><a href="#" onClick={this.props.onUndo}><i className="fa fa-undo"/></a></li>
-          <li><a href="#" onClick={this.props.onRedo}><i className="fa fa-repeat"/></a></li>
-          <li><a href="#" onClick={this.props.onZoomIn}><i className="fa fa-search-plus"/></a></li>
-          <li><a href="#" onClick={this.props.onZoomOut}><i className="fa fa-search-minus"/></a></li>
+          <li><a href="#" onClick={this.props.onUndo}><i className="icon-left"/></a></li>
+          <li><a href="#" onClick={this.props.onRedo}><i className="icon-right"/></a></li>
+          <li><a href="#" onClick={this.props.onZoomIn}><i className="icon-zoom-in"/></a></li>
+          <li><a href="#" onClick={this.props.onZoomOut}><i className="icon-zoom-out"/></a></li>
           {/*<li><a href="#" onClick={this.props.onContain}><i className="fa fa-arrows-alt"/></a></li>*/}
         </ul>
         <ul className={styles.left}>
-          <li className={this.props.cursorMode == CursorMode.MULTISELECT ? styles.active : ''}><a href="#" onClick={this.props.onMultiselectMode}><i className="fa fa-mouse-pointer"/></a></li>
-          <li className={this.props.cursorMode == CursorMode.PAN  ? styles.active : ''}><a href="#" onClick={this.props.onPanMode}><i className="fa fa-hand-pointer-o"/></a></li>
+          <li className={this.props.cursorMode == CursorMode.MULTISELECT ? styles.active : ''}><a href="#" onClick={this.props.onMultiselectMode}><i className="icon-hand"/></a></li>
+          <li className={this.props.cursorMode == CursorMode.PAN  ? styles.active : ''}><a href="#" onClick={this.props.onPanMode}><i className="icon-cursor"/></a></li>
           <li><a href="#"><i className="fa fa-object-group"/></a></li>
         </ul>
 
         <ul className={styles.right}>
-          <li className={this.props.showCodeView ? styles.active : ''}><a href="#" onClick={this.props.onCodeViewToggle}><i className="fa fa-code"/></a></li>
-          <li><a href="#"><i className="fa fa-cog"/></a></li>
+          <li className={this.props.showCodeView ? styles.active : ''}><a href="#" onClick={this.props.onCodeViewToggle}><i className="icon-code"/></a></li>
+          <li><a href="#"><i className="icon-settings"/></a></li>
         </ul>
       </div>
     );
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -54,11 +54,11 @@ export default validate(merge(baseConfig, {
         ]
       },
 
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.woff(\?.*)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff2(\?.*)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?.*)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?.*)?$/, loader: 'file' },
+      { test: /\.svg(\?.*)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
     ]
   },
 
