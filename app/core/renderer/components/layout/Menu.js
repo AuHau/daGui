886186891dae26444fb6bb2d5537fa6932027d36
Adam Uhlir <uhlir.a@gmail.com>
2017-05-15 14:01:31 +0200

    Adding tooltips to Menu's items

--- app/core/renderer/components/layout/Menu.js
+++ app/core/renderer/components/layout/Menu.js
@@ -8,6 +8,8 @@ import * as uiActions from 'shared/actions/ui';
 import * as graphActions from 'shared/actions/graph';
 import * as fileActions from 'shared/actions/file';
 
+import Tooltip from 'react-tooltip';
+
 class Menu extends Component {
 
   componentDidMount(){
@@ -50,33 +52,34 @@ class Menu extends Component {
     return (
       <div className={styles.container}>
         <ul className={styles.left}>
-          <li><a href="#" onClick={this.props.onNew}><i className="icon-file"/></a></li>
-          <li><a href="#" onClick={this.props.onOpen}><i className="icon-open"/></a></li>
-          <li><a href="#" onClick={this.props.onSave}><i className="icon-save"/></a></li>
-          <li><a href="#"><i className="icon-screenshot"/></a></li>
+          <li><a href="#" onClick={this.props.onNew} data-tip="New file<br><span class='shortcut'>(Ctrl+N)</span>"><i className="icon-file"/></a></li>
+          <li><a href="#" onClick={this.props.onOpen} data-tip="Open file<br><span class='shortcut'>(Ctrl+O)</span>"><i className="icon-open"/></a></li>
+          <li><a href="#" onClick={this.props.onSave} data-tip="Save file<br><span class='shortcut'>(Ctrl+S)</span>"><i className="icon-save"/></a></li>
+          <li><a href="#" data-tip="Save image of the graph<br><span class='shortcut'>(Ctrl+G)</span>"><i className="icon-screenshot"/></a></li>
         </ul>
         <ul className={styles.left}>
-          <li><a href="#" onClick={this.props.onCopy}><i className="icon-copy"/></a></li>
-          <li><a href="#" onClick={this.props.onCut}><i className="icon-take-out"/></a></li>
-          <li><a href="#" onClick={this.props.onPaste}><i className="icon-paste"/></a></li>
+          <li><a href="#" onClick={this.props.onCopy} data-tip="Copy node(s)<br><span class='shortcut'>(Ctrl+C)</span>"><i className="icon-copy"/></a></li>
+          <li><a href="#" onClick={this.props.onCut} data-tip="Cut node(s)<br><span class='shortcut'>(Ctrl+X)</span>"><i className="icon-take-out"/></a></li>
+          <li><a href="#" onClick={this.props.onPaste} data-tip="Paste node(s)<br><span class='shortcut'>(Ctrl+V)</span>"><i className="icon-paste"/></a></li>
         </ul>
         <ul className={styles.left}>
-          <li><a href="#" onClick={this.props.onUndo}><i className="icon-left"/></a></li>
-          <li><a href="#" onClick={this.props.onRedo}><i className="icon-right"/></a></li>
-          <li><a href="#" onClick={this.props.onZoomIn}><i className="icon-zoom-in"/></a></li>
-          <li><a href="#" onClick={this.props.onZoomOut}><i className="icon-zoom-out"/></a></li>
+          <li><a href="#" onClick={this.props.onUndo} data-tip="Undo<br><span class='shortcut'>(Ctrl+Z)</span>"><i className="icon-left"/></a></li>
+          <li><a href="#" onClick={this.props.onRedo} data-tip="Redo<br><span class='shortcut'>(Ctrl+Shift+Z)</span>"><i className="icon-right"/></a></li>
+          <li><a href="#" onClick={this.props.onZoomIn} data-tip="Zoom in<br><span class='shortcut'>(Ctrl+Mouse Wheel)</span>"><i className="icon-zoom-in"/></a></li>
+          <li><a href="#" onClick={this.props.onZoomOut} data-tip="Zoom out<br><span class='shortcut'>(Ctrl+Mouse Wheel)</span>"><i className="icon-zoom-out"/></a></li>
           {/*<li><a href="#" onClick={this.props.onContain}><i className="fa fa-arrows-alt"/></a></li>*/}
         </ul>
         <ul className={styles.left}>
-          <li className={this.props.cursorMode == CursorMode.MULTISELECT ? styles.active : ''}><a href="#" onClick={this.props.onMultiselectMode}><i className="icon-hand"/></a></li>
-          <li className={this.props.cursorMode == CursorMode.PAN  ? styles.active : ''}><a href="#" onClick={this.props.onPanMode}><i className="icon-cursor"/></a></li>
-          <li><a href="#"><i className="fa fa-object-group"/></a></li>
+          <li className={this.props.cursorMode == CursorMode.MULTISELECT ? styles.active : ''}><a href="#" onClick={this.props.onMultiselectMode} data-tip="Selection mode"><i className="icon-hand"/></a></li>
+          <li className={this.props.cursorMode == CursorMode.PAN  ? styles.active : ''}><a href="#" onClick={this.props.onPanMode} data-tip="Panning mode"><i className="icon-cursor"/></a></li>
+          <li><a href="#" data-tip="Group nodes"><i className="fa fa-object-group"/></a></li>
         </ul>
 
         <ul className={styles.right}>
-          <li className={this.props.showCodeView ? styles.active : ''}><a href="#" onClick={this.props.onCodeViewToggle}><i className="icon-code"/></a></li>
-          <li><a href="#"><i className="icon-settings"/></a></li>
+          <li className={this.props.showCodeView ? styles.active : ''}><a href="#" onClick={this.props.onCodeViewToggle}  data-tip="Display/hide code view"><i className="icon-code"/></a></li>
+          <li><a href="#" data-tip="Settings"><i className="icon-settings"/></a></li>
         </ul>
+        <Tooltip place="bottom" type="dark" effect="solid" delayShow={850} className={styles.tooltip} offset={{'top': -13, 'left': -10}} html={true}/>
       </div>
     );
   }
